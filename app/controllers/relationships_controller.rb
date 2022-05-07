3f1173f876395ac968e33aa5ceb58ef1f0f91b93
namamoto <emaraku1009@gmail.com>
2022-05-07 07:19:26 +0000

    [End]Ouyoukadai4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,32 +1,25 @@
 class RelationshipsController < ApplicationController
-     before_action :set_user
+  before_action :authenticate_user!
 
   def create
-    following = current_user.follow(@user)
-    if following.save
-      flash[:success] = 'ユーザーをフォローしました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to @user
-    end
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+		redirect_to request.referer
   end
 
   def destroy
-    following = current_user.unfollow(@user)
-    if following.destroy
-      flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to @user
-    else
-      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to @user
-    end
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+		redirect_to request.referer
   end
 
-  private
-    def
-      @user = User.find(params[:relationship][:follow_id])
-    end
+  def followings
+    user = User.find(params[:user_id])
+		@users = user.followings
+  end
 
+  def followers
+    user = User.find(params[:user_id])
+		@users = user.followers
+  end
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,12 +22,12 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   # フォローしたときの処理
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
   # フォローを外すときの処理
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
   # フォローしているか判定
   def following?(user)
