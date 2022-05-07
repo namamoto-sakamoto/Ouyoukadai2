1cd4c28b2024b055ff62167d8d4a9c05aa55f8e8
namamoto <emaraku1009@gmail.com>
2022-05-07 01:11:42 +0000

    [Start]Ouyoukadai4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
 
   root to: "homes#top"
@@ -8,5 +10,10 @@ Rails.application.routes.draw do
    resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
  end
- resources :users, only: [:create, :index, :show, :edit, :update]
+
+ resources :users, only: [:create, :index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+ end
 end
